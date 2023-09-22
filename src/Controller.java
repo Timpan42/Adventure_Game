28982f381d618192a5df7a14f12507b36006f30e
Timpan42 <tim@fagerdal.se>
2023-09-22 11:09:01 +0200

    Programet funkar i terminalen, men texten är lite lång

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,59 @@
+import com.google.gson.JsonArray;
+
+import java.util.Scanner;
+
 public class Controller {
+    Model model;
+    boolean gameIsRun;
+
+    public void choses(Model model){
+        JsonArray options = model.getOptions();
+        for (int i = 0; i < options.size(); i++) {
+            model.setChose(i);
+            model.setChoseText();
+            System.out.println("[" + i + "]" + " " + model.getChoseText());
+        }
+    }
+
+    public void sendNextId(Model model, int nextId){
+        model.setChose(nextId);
+        model.setChoseNextId();
+    }
+
+    public void gamePlaying(Model model){
+        if(model.getChoseNextId() < 0) {
+            System.out.println("Game Over");
+            gameIsRun = false;
+        } else {
+            model.setITO();
+            System.out.println("Title: " + model.getTitle());
+
+            choses(model);
+        }
+    }
+
+    public void gamePlay(boolean state){
+        gameIsRun = state;
+    }
+
+    public Controller(Model model, String file) {
+        this.model = model;
+        model.setFile(file);
+        model.startGame();
+    }
+
+
+
+    public static void main(String[] args) {
+        String testFile = "src/story.json";
+        Model m = new Model();
+        Controller c = new Controller(m, testFile);
+        while (true){
+            c.gamePlaying(m);
+            Scanner sc = new Scanner(System.in);
+            int nextId = sc.nextInt();
+            c.sendNextId(m,nextId);
+
+        }
+    }
 }
--- src/Model.java
+++ src/Model.java
@@ -1,9 +1,9 @@
 
-import com.google.gson.Gson;
+import com.google.gson.*;
+
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.Iterator;
 
 /*
  How to use Gson
@@ -11,10 +11,112 @@ import java.util.Iterator;
  https://howtodoinjava.com/gson/gson/
  */
 public class Model {
-    public static void main(String[] args) throws IOException {
-        Gson gson = new Gson();
-        Object obj = gson.fromJson(new FileReader("src/name.json"), Object.class);
+    String file;
+    Gson gson = new Gson();
+    JsonElement id;
+    String title;
+    String text;
+    JsonArray options;
+    JsonObject chose;
+    String choseText;
+    int choseNextId;
 
-        System.out.println(obj);
+    public void startGame(){
+        choseNextId = 0;
     }
+    public void setFile(String file){
+        this.file = file;
+    }
+
+    public String getFile(){
+        return file;
+    }
+
+    public JsonObject[] getJson(){
+        JsonObject[] arrObj;
+        try {
+             arrObj = gson.fromJson(new FileReader(file), JsonObject[].class);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return arrObj;
+    }
+
+    public void setId(JsonObject[] arrObj){
+        this.id = arrObj[choseNextId].get("id");
+    }
+    public JsonElement getId(){
+        return id;
+    }
+
+    public void setTitle(JsonObject[] arrObj){
+        this.title = arrObj[choseNextId].get("title").getAsString();
+    }
+    public String getTitle(){
+        return title;
+    }
+
+    public void setText(JsonObject[] arrObj){
+        this.text = arrObj[choseNextId].get("text").getAsString();
+    }
+    public String getText(){
+        return text;
+    }
+
+    public void setOptions (JsonObject[] arrObj){
+        options = arrObj[choseNextId].get("options").getAsJsonArray();
+    }
+    public JsonArray getOptions(){
+        return options;
+    }
+
+    public void setChose(int index){
+        chose  = options.get(index).getAsJsonObject();
+    }
+    public JsonElement getChose(){
+        return chose;
+    }
+
+    public void setChoseText(){
+        choseText  = chose.get("text").getAsString();
+    }
+    public String getChoseText(){
+        return choseText;
+    }
+
+    public void setChoseNextId(){
+        choseNextId  = chose.get("nextId").getAsInt();
+    }
+    public int getChoseNextId(){
+        return choseNextId;
+    }
+
+    public void setITO(){
+        JsonObject[] arrObj = getJson();
+        setId(arrObj);
+        setTitle(arrObj);
+        setText(arrObj);
+        setOptions(arrObj);
+    }
+
+    public void printJson(){
+        System.out.println("Id: " + id);
+        System.out.println("Title: " + title);
+        System.out.println("Options: " + options);
+        System.out.println("Chose: " + chose);
+        System.out.println("Chose text: " + choseText);
+        System.out.println("Chose next id: " + choseNextId);
+
+    }
+
+/*    public static void main(String[] args) throws IOException {
+        Model m = new Model();
+        String testFile = "src/test.json";
+        String storyFile = "src/story.json";
+        m.setFile(testFile);
+        m.setJson();
+        m.printJson();
+
+
+    }*/
 }
